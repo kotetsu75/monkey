9bb4455f139a8613a6d1012daf5a19c71f1fda83
kotetsu75 <tsuji.daisuke75@gmail.com>
2021-12-19 15:43:16 +0900

    2.5 completed

--- parser/parser.go
+++ parser/parser.go
@@ -61,6 +61,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -80,6 +82,19 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	}
 
 	// TODO: セミコロンに遭遇するまで指揮を読み飛ばしてしまっている
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+
+	p.nextToken()
+
+	// TODO: セミコロンに遭遇するまで式を読み飛ばしてしまっている
 	for !p.curTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
